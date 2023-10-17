12aba101f3c10ea8f9116de30b60c546a42c1155
Joseph Kogut <joseph@balena.io>
2023-10-17 14:15:34 -0700

    use argp for argument parsing
    Change-type: patch
    Signed-off-by: Joseph Kogut <joseph@balena.io>

--- tcgtool.c
+++ tcgtool.c
@@ -9,6 +9,7 @@
 #include <assert.h>
 #include <sys/stat.h>
 #include <errno.h>
+#include <argp.h>
 
 #define MIN(a, b) (a < b) ? a : b
 
@@ -47,19 +48,7 @@ int parse_guid(struct EFI_GUID *g, const char *s)
 	return -EINVAL;
 }
 
-void print_usage(char **argv)
-{
-	printf("usage: %s efivar_path\n", argv[0]);
-}
-
-int main(int argc, char **argv)
-{
-	if (argc < 2) {
-		print_usage(argv);
-		exit(0);
-	}
-
-	char *efivar_path = argv[1];
+int measure_efivar (const char *efivar_path) {
 	const int exists = access(efivar_path, F_OK) == 0;
 	const size_t tmpsz = 0x400;
 	char tmp[tmpsz];
@@ -135,6 +124,61 @@ int main(int argc, char **argv)
 	size_t written = write(STDOUT_FILENO, VarLog, VarLogSize);
 	if (written != VarLogSize)
 		return 1;
+
+	return 0;
+}
+
+enum action {
+	ACTION_NONE,
+	ACTION_MEASURE_EFIVAR,
+};
+
+struct tcgtool_arguments {
+	enum action action;
+	char *path;
+};
+
+static error_t parse_opt(int key, char *arg, struct argp_state *state)
+{
+	struct tcgtool_arguments *args = state->input;
+	switch (key) {
+		case 'e':
+			args->action = ACTION_MEASURE_EFIVAR;
+			args->path = arg;
+			break;
+	}
+
+	return 0;
+}
+
+static struct argp_option options[] = {
+	{ 0, 0, 0, 0, "Measurements:" },
+	{ "measure-efivar", 'e', "PATH", 0, "Measure an efivar, outputting the binary"
+		" contents in an EFI_VARIABLE_DATA struct for hashing (default)" },
+	{ 0 },
+};
+
+static char doc[] = "Measure and output data in accordance with the TCG PC"
+	" Client Platform Firmware Profile specification";
+static struct argp argp = { options, parse_opt, 0, doc, 0, 0, 0 };
+
+int main(int argc, char **argv)
+{
+	struct tcgtool_arguments args = {
+		.action = argc == 2 ? ACTION_MEASURE_EFIVAR : ACTION_NONE,
+		.path = argv[1],
+	};
+
+	argp_parse(&argp, argc, argv, 0, 0, &args);
+
+	switch (args.action) {
+		case ACTION_MEASURE_EFIVAR:
+			measure_efivar(args.path);
+			break;
+		default:
+			printf("No action specified\n");
+			break;
+	}
 
 	return 0;
 }
